2a5d35f162bc652f44460a2106d08efe2674eb1b
Emil-87 <emil.pegas@gmail.com>
2022-11-28 01:35:01 +0600

    task6

---
+++ task6/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Задача 6. Напишите прогу,которая на вход принимает число и выдает,
+        //является ли оно четным(делится ли оно на 2 без остатка)
+        int a;
+
+        Console.Write("Введите число :");
+        a = Convert.ToInt32(Console.ReadLine());
+        if (a % 2 == 0)
+        {
+            Console.Write("Введенное число четное");
+        }
+        else
+        {
+            Console.Write("Введенное число не четное");
+        }
+    }
+}
