77d3c7e4e0311ce027151b31caf64afe5a4ed27a
Emil-87 <emil.pegas@gmail.com>
2022-11-30 01:10:33 +0600

    task10.

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 10. Напишите прогу, при вводе 3х значного числа должна показывать 2ю цифру.
+
+Console.WriteLine("Введите 3-х значное число:");
+int a = 0;
+a = Convert.ToInt32(Console.ReadLine());
+if (a >= 100 && a <= 999)
+{
+    int DecreaseByTen = a / 10;
+    int LastDigit = DecreaseByTen % 10;
+    Console.WriteLine("Вторая цифра числа=" + LastDigit);
+}
+else
+{
+    Console.WriteLine("Ошибка, Вы ввели неправильное число!");
+}
